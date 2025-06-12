7996aced418e361c314b3cfb6b1c0d5318905f57
rbantug <ronbantug22@gmail.com>
2025-06-12 21:22:44 +0800

    feat: incorporated camera, player and renderer to the scene. Added lighting as well.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import * as THREE from "three";
-import { Renderer } from "./components/Renderer";
-import { Camera } from "./components/Camera";
-import { player } from "./components/Player";
 import "./style.css";
 
+import * as THREE from "three";
+
+import { Camera } from "./components/Camera";
+import { Player } from "./components/Player";
+import { Renderer } from "./components/Renderer";
+
 const scene = new THREE.Scene();
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+const dirLight = new THREE.DirectionalLight()
+dirLight.position.set(-100, -100, 200)
+scene.add(dirLight)
+
+const player = Player()
 scene.add(player);
 
 const camera = Camera();
@@ -13,4 +22,4 @@ scene.add();
 const renderer = Renderer();
 renderer.render(scene, camera);
 
-// 3:16
+// 10:41
