903ec4f8aebc3f97c6d809952109c4c8796830c5
rbantug <ronbantug22@gmail.com>
2025-06-12 21:21:52 +0800

    feat: added Camera.js, Player.js and Renderer.js components

---
+++ src/components/Player.js
@@ -0,0 +1,14 @@
+import * as THREE from "three"
+
+export function Player() {
+    const body = new THREE.Mesh(
+        new THREE.BoxGeometry(15,15,20),
+        new THREE.MeshLambertMaterial({
+            color: "white",
+            flatShading: true
+        })
+    )
+    body.position.z = 10
+
+    return body
+}
