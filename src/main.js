387566ab44d8a45bff485dad4d4ad231fcb670b7
rbantug <ronbantug22@gmail.com>
2025-06-16 14:26:05 +0800

    feat: updated main.js to add map component to the scene

--- src/main.js
+++ src/main.js
@@ -3,23 +3,31 @@ import "./style.css";
 import * as THREE from "three";
 
 import { Camera } from "./components/Camera";
+import { initMap, map } from "./components/Map";
 import { Player } from "./components/Player";
 import { Renderer } from "./components/Renderer";
 
 const scene = new THREE.Scene();
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight)
-const dirLight = new THREE.DirectionalLight()
-dirLight.position.set(-100, -100, 200)
-scene.add(dirLight)
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, 200);
+scene.add(dirLight);
 
-const player = Player()
+const player = Player();
 scene.add(player);
 
+scene.add(map)
+
 const camera = Camera();
-scene.add();
+
+function initGame() {
+    initMap()
+}
+
+initGame()
 
 const renderer = Renderer();
 renderer.render(scene, camera);
 
-// 10:41
+// 17:01
