82e7c6587a2ed1be62ffbdebcb51de3b98abc14c
rbantug <ronbantug22@gmail.com>
2025-06-20 11:17:15 +0800

    feat: added AnimateVehicles.js

---
+++ src/AnimateVehicles.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+
+import { metadata as rows } from "./components/Map";
+import { maxTileIndex, minTileIndex, tileSize } from "./constants";
+
+const clock = new THREE.Clock();
+
+export function animateVehicles() {
+  const delta = clock.getDelta();
+  //const delta = clock.getElapsedTime();
+
+  rows.forEach((rowData) => {
+    if (rowData.type === "car" || rowData.type === "truck") {
+      const beginningOfRow = (minTileIndex - 2) * tileSize;
+      const endOfRow = (maxTileIndex + 2) * tileSize;
+
+      rowData.vehicles.forEach(({ ref }) => {
+        if (!ref) throw Error("Vehicle reference is missing");
+
+        if (rowData.direction) {
+          ref.position.x =
+            ref.position.x > endOfRow
+              ? beginningOfRow
+              : ref.position.x + rowData.speed * delta;
+        } else {
+          ref.position.x =
+            ref.position.x < beginningOfRow
+              ? endOfRow
+              : ref.position.x - rowData.speed * delta;
+        }
+      });
+    }
+  });
+}
